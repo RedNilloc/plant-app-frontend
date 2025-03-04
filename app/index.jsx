24e11b181369f50e3af77c75645b28603e46bb32
“Hayp” <hanna.y.peters@gmail.com>
2025-03-04 16:07:26 +0000

    some tinkering

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,6 @@
 import SearchPage from "./pages/searchPage";
+import WelcomePge from "./pages/welcomePage";
 
 export default function Index() {
-  return <SearchPage></SearchPage>;
+  return <WelcomePge></WelcomePge>;
 }
