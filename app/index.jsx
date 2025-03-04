75baa1b0c17cc34ddabbb5a0dcc01149d7d21799
“Hayp” <hanna.y.peters@gmail.com>
2025-03-04 14:50:46 +0000

    fixed some imports

--- app/index.jsx
+++ app/index.jsx
@@ -1,31 +1,5 @@
-import { Text, View, SafeAreaView, ScrollView, StyleSheet } from "react-native"
-import PZHeader from "./components/header"
-import PZFooter from "./components/footer"
-import Search from "./pageContents/search"
+import SearchPage from "./pages/searchPage";
 
 export default function Index() {
-    return (
-        <View style={{ height: "100%", backgroundColor: "#ffffff" }}>
-            <SafeAreaView style={styles.sav}>
-                <ScrollView bounces={false}>
-                    <PZHeader>Search</PZHeader>
-                    <View style={styles.pageContent}>
-                        <Search />
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-            <PZFooter />
-        </View>
-    )
+  return <SearchPage></SearchPage>;
 }
-
-const styles = StyleSheet.create({
-    sav: { flex: 0, backgroundColor: "#222926" },
-    pageContent: {
-        paddingTop: "10%",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#ffffff",
-        paddingBottom: "20%",
-    },
-})
--- app/pages/searchPage.jsx
+++ app/pages/searchPage.jsx
@@ -1,31 +1,31 @@
-import { Text, View, StyleSheet, Button } from "react-native"
-import PZFooter from "../components/footer"
-import PZHeader from "../components/header"
-import Search from "../pageContents/search"
+import { Text, View, SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import PZFooter from "../components/footer";
+import PZHeader from "../components/header";
+import Search from "../pageContents/search";
 
 export default function searchPage() {
-    return (
-        <View style={{ height: "100%", backgroundColor: "#ffffff" }}>
-            <SafeAreaView style={styles.sav}>
-                <ScrollView bounces={false}>
-                    <PZHeader>Search</PZHeader>
-                    <View style={styles.pageContent}>
-                        <Search />
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-            <PZFooter />
-        </View>
-    )
+  return (
+    <View style={{ height: "100%", backgroundColor: "#ffffff" }}>
+      <SafeAreaView style={styles.sav}>
+        <ScrollView bounces={false}>
+          <PZHeader>Search</PZHeader>
+          <View style={styles.pageContent}>
+            <Search />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+      <PZFooter />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 20,
-    },
-    text: {
-        fontSize: 20,
-        fontWeight: "bold",
-        alignSelf: "center",
-    },
-})
+  container: {
+    marginTop: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+});
