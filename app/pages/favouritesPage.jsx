f9d0d3e269fa5b4cbe1349021d78fe53203d6967
Patrick-jc-Evans <paddy@paddyevans.com>
2025-03-05 15:40:33 +0000

    create favourites page with placeholder cards

---
+++ app/pages/favouritesPage.jsx
@@ -0,0 +1,32 @@
+import { Text, View, SafeAreaView, ScrollView } from "react-native"
+import PZHeader from "../components/header"
+import PZFooter from "../components/footer"
+import { StyleSheet } from "react-native"
+import Favourites from "../pageContents/favourites"
+
+export default function FavouritesPage() {
+    return (
+        <View style={{ height: "100%", backgroundColor: "#ffffff" }}>
+            <SafeAreaView style={styles.sav}>
+                <ScrollView bounces={false}>
+                    <PZHeader>Favourites</PZHeader>
+                    <View style={styles.pageContent}>
+                        <Favourites></Favourites>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+            <PZFooter />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    sav: { flex: 0, backgroundColor: "#222926" },
+    pageContent: {
+        paddingTop: "10%",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ffffff",
+        paddingBottom: "20%",
+    },
+})
