87674e64cdd6ad573522f126dcc9f9508390e862
“Hayp” <hanna.y.peters@gmail.com>
2025-03-06 15:03:05 +0000

    scroll on index

--- app/index.jsx
+++ app/index.jsx
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   StyleSheet,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import { useRouter } from "expo-router";
 
@@ -11,72 +12,74 @@ export default function Index() {
   const router = useRouter();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Temporary navigation</Text>
-      <View>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/welcomePage")}
-        >
-          <Text style={styles.buttonText}>Welcome page</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/loginPage")}
-        >
-          <Text style={styles.buttonText}>Login page</Text>
-        </TouchableOpacity>
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Temporary navigation</Text>
+        <View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/welcomePage")}
+          >
+            <Text style={styles.buttonText}>Welcome page</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/loginPage")}
+          >
+            <Text style={styles.buttonText}>Login page</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/homePage")}
-        >
-          <Text style={styles.buttonText}>Home page</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/searchPage")}
-        >
-          <Text style={styles.buttonText}>Search page</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/yourPlantsPage")}
-        >
-          <Text style={styles.buttonText}>Your Plants page</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/favouritesPage")}
-        >
-          <Text style={styles.buttonText}>Favourites</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/notificationsPage")}
-        >
-          <Text style={styles.buttonText}>Your Notifications page</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/searchResults")}
-        >
-          <Text style={styles.buttonText}>Search results page</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/individualPlantPage")}
-        >
-          <Text style={styles.buttonText}>Individual plant page</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push("/pages/signupPage")}
-        >
-          <Text style={styles.buttonText}>Signup page</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/homePage")}
+          >
+            <Text style={styles.buttonText}>Home page</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/searchPage")}
+          >
+            <Text style={styles.buttonText}>Search page</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/yourPlantsPage")}
+          >
+            <Text style={styles.buttonText}>Your Plants page</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/favouritesPage")}
+          >
+            <Text style={styles.buttonText}>Favourites</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/notificationsPage")}
+          >
+            <Text style={styles.buttonText}>Your Notifications page</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/searchResults")}
+          >
+            <Text style={styles.buttonText}>Search results page</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/individualPlantPage")}
+          >
+            <Text style={styles.buttonText}>Individual plant page</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/pages/signupPage")}
+          >
+            <Text style={styles.buttonText}>Signup page</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 
