c20c015aa67e1fa537df6079c8b4c578a454fd52
“Hayp” <hanna.y.peters@gmail.com>
2025-03-07 12:09:14 +0000

    failed attempt at buttons:

---
+++ app/components/buttons/touchableOpacity.jsx
@@ -0,0 +1,32 @@
+// DOES NOT WORK DO NOT USE
+
+// import { useFonts } from "expo-font";
+// import { FugazOne_400Regular } from "@expo-google-fonts/fugaz-one";
+// import { Text, StyleSheet, TouchableOpacity } from "react-native";
+
+// export default function StandardTouchableOpacity({ title, func }) {
+//   const [fontsLoaded] = useFonts({
+//     FugazOne_400Regular,
+//   });
+//   return (
+//     <TouchableOpacity style={styles.button}>
+//       <Text style={styles.text}>{title}</Text>
+//     </TouchableOpacity>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   button: {
+//     backgroundColor: "#8EC255",
+//     margin: 20,
+//     padding: 10,
+//     width: 220,
+//     alignSelf: "center",
+//   },
+//   text: {
+//     color: "#ffffff",
+//     alignSelf: "center",
+//     fontFamily: "FugazOne_400Regular",
+//     fontSize: 30,
+//   },
+// });
