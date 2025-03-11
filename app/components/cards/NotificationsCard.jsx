590c091d78947cd144791f3f1f567fae96032e03
RedNilloc <coregond11@gmail.com>
2025-03-11 08:44:24 +0000

    update notificiations stuff with api integration

--- app/components/cards/NotificationsCard.jsx
+++ app/components/cards/NotificationsCard.jsx
@@ -4,28 +4,79 @@ import { Inter_400Regular, Inter_300Light } from "@expo-google-fonts/inter";
 import { KronaOne_400Regular } from "@expo-google-fonts/krona-one";
 import { useFonts } from "expo-font";
 import { Button } from "@react-navigation/elements";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function NotificationsCard({
-  plantName,
-  lastWatered,
-  needsWatered,
-  plantImage,
+  user_id,
+  user,
+  plant,
+  zone,
+  last_watered,
 }) {
+  const [ownedPlants, setOwnedPlants] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const [fontsLoaded] = useFonts({
     Inter_300Light,
     FugazOne_400Regular,
     KronaOne_400Regular,
   });
 
+  useEffect(() => {
+    axios
+      .get(
+        `https://plant-app-backend-87sk.onrender.com/api/users/${user_id}/owned_plants`
+      )
+      .then((response) => {
+        const currentDay = new Date();
+        const testWateredDatePlants = response.data.plants.map((plant) => {
+          const lastWateredDate = new Date(plant.last_watered) || "2025-02-01";
+          const timeDifference = currentDay.getTime() - lastWateredDate.getTime();
+          const dayDifference = timeDifference / (1000 * 3600 * 24);
+
+          return {
+            ...plant,
+            last_watered: plant.last_watered || "2025-02-01",
+            daysSinceWatered: dayDifference,
+          };
+        });
+        setOwnedPlants(testWateredDatePlants);
+        console.log(testWateredDatePlants);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError("Failed to load plants");
+      });
+  }, [user_id]);
+
+  console.log(ownedPlants);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Image style={styles.thumbnail} source={plantImage} /> 
       <View style={styles.textContents}>
         <Text style={styles.titleText}>Important!</Text>
-        <Text style={styles.lineOne}>
-          Oh no! I'm afraid it's been {lastWatered - needsWatered} day(s) since
-          you watered your {plantName}!
-        </Text>
+        {ownedPlants.length > 0 ? (
+          ownedPlants.map((plant, index) => {
+            const needsWater = plant.daysSinceWatered > 7;
+            console.log(
+              `Plant: ${plant.common_name}, Last Watered: ${plant.last_watered}, Days Since: ${plant.daysSinceWatered}`
+            );
+            return (
+              <Text key={index} style={styles.lineOne}>
+                {needsWater
+                  ? `Oh no! I'm afraid you haven't watered your ${plant.common_name} since ${plant.last_watered}!`
+                  : `${plant.common_name} is allllllright!`}
+              </Text>
+            );
+          })
+        ) : (
+          <Text style={styles.lineOne}>
+            You've not selected any plants yet, so no notification 4 u, fwiend!
+          </Text>
+        )}
       </View>
       <Button style={styles.button}></Button>
     </SafeAreaView>
--- app/pages/notificationsPage.jsx
+++ app/pages/notificationsPage.jsx
@@ -4,15 +4,8 @@ import PZFooter from "../components/footer";
 import { StyleSheet } from "react-native";
 import NotificationsCard from "../components/cards/NotificationsCard.jsx";
 
-export default function NotificationsPage() {
-
-  const exampleNotificationData = {
-    plantName: "geranium",
-    lastWatered: 4,
-    needsWatered: 3,
-    plantImage: require("../../assets/images/geranium.jpg")
-  };
-
+export default function NotificationsPage(user_id) {
+  const testUserId = 2;
 
   return (
     <View style={{ height: "100%", backgroundColor: "#ffffff" }}>
@@ -21,7 +14,7 @@ export default function NotificationsPage() {
           <PZHeader>Notifications</PZHeader>
           <View style={styles.pageContent}>
             <Text>This is a notifications page!</Text>
-            <NotificationsCard {...exampleNotificationData}/>
+            <NotificationsCard user_id={testUserId} />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -41,5 +34,13 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
+// we need to get the user's plants
+// we need to get plant by id to check its watering value
+// we need to check the last-watered date value
+// we need to compare that value to the current date
+// if the time difference exceeds the time that plant should be watered, the user is sent a notification
+// if, through getting the weather api, there has been rain in the user's location during this period,
+// the "last watered" date is set to the date of the rain
+// we need an array of the plants for whom a notification is warranted
+// these are the ones which are send onto the card
+// the card should probably only show a single plant at a time, do we need a component for userplants?
