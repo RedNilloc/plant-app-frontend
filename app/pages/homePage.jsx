05e54ae149d1b6b1993c3eaacbd05af786ebfd5a
Patrick-jc-Evans <paddy@paddyevans.com>
2025-03-12 12:32:58 +0000

    update styling on home page

--- app/pages/homePage.jsx
+++ app/pages/homePage.jsx
@@ -1,41 +1,37 @@
-import { Text, View, SafeAreaView, ScrollView } from "react-native";
-import PZHeader from "../components/header";
-import PZFooter from "../components/footer";
-import SignedInView from "../pageContents/home";
-import { StyleSheet } from "react-native";
-import { useUser } from "../contexts/userContext";
+import { Text, View, SafeAreaView, ScrollView } from "react-native"
+import PZHeader from "../components/header"
+import PZFooter from "../components/footer"
+import SignedInView from "../pageContents/home"
+import { StyleSheet } from "react-native"
+import { useUser } from "../contexts/userContext"
 
 export default function Home() {
-  const { user } = useUser();
-  return (
-    <View style={{ height: "100%", backgroundColor: "#ffffff" }}>
-      <SafeAreaView style={styles.sav}>
-        <ScrollView bounces={false}>
-          <PZHeader>Home</PZHeader>
-          <View style={styles.pageContent}>
-            <Text style={styles.white}>
-              Currently logged in as {user.username}
-            </Text>
-            <SignedInView></SignedInView>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-      <PZFooter />
-    </View>
-  );
+    return (
+        <View style={{ height: "100%", backgroundColor: "#ffffff" }}>
+            <SafeAreaView style={styles.sav}>
+                <ScrollView bounces={false}>
+                    <PZHeader>Home</PZHeader>
+                    <View style={styles.pageContent}>
+                        <SignedInView />
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+            <PZFooter />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  sav: { flex: 0, backgroundColor: "#222926" },
-  pageContent: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#222926",
-    paddingBottom: "20%",
-  },
-  white: {
-    color: "white",
-    margin: 3,
-    marginTop: 7,
-  },
-});
+    sav: { flex: 0, backgroundColor: "#222926" },
+    pageContent: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ffffff",
+        paddingBottom: "20%",
+    },
+    white: {
+        color: "white",
+        margin: 3,
+        marginTop: 7,
+    },
+})
