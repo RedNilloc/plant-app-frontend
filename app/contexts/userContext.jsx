edc5f690dba37d1991d8aa608d98996ea009e7ff
Patrick-jc-Evans <paddy@paddyevans.com>
2025-03-12 15:36:46 +0000

    fix userId bug

--- app/contexts/userContext.jsx
+++ app/contexts/userContext.jsx
@@ -1,27 +1,27 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react"
 
 const UserContext = createContext({
-  user: {
-    id: 2,
-    username: "IgnatiusTaurus",
-    email: "Ignatius_Thiel@yahoo.com",
-    location: "[51.65517222915666, -1.6943713200557795]",
-  },
-});
+    user: {
+        id: 2,
+        username: "IgnatiusTaurus",
+        email: "Ignatius_Thiel@yahoo.com",
+        location: "[51.65517222915666, -1.6943713200557795]",
+    },
+})
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    id: 3,
-    username: "IgnatiusTaurus",
-    email: "Ignatius_Thiel@yahoo.com",
-    location: "[51.65517222915666, -1.6943713200557795]",
-  });
+    const [user, setUser] = useState({
+        id: 2,
+        username: "IgnatiusTaurus",
+        email: "Ignatius_Thiel@yahoo.com",
+        location: "[51.65517222915666, -1.6943713200557795]",
+    })
 
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
 
-export const useUser = () => useContext(UserContext);
+export const useUser = () => useContext(UserContext)
