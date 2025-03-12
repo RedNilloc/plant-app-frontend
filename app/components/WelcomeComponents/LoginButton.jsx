fea50d7370c5ec79084e6569c55bbb57e08f6bc0
Patrick-jc-Evans <paddy@paddyevans.com>
2025-03-12 14:47:35 +0000

    fix welcome page styling issues

--- app/components/WelcomeComponents/LoginButton.jsx
+++ app/components/WelcomeComponents/LoginButton.jsx
@@ -1,37 +1,45 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
-import { useRouter } from "expo-router";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native"
+import { useRouter } from "expo-router"
+import { FugazOne_400Regular } from "@expo-google-fonts/fugaz-one"
+import { useFonts } from "expo-font"
 
 export default function LoginButton() {
-  const router = useRouter();
+    const router = useRouter()
 
-  return (
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => router.push("/pages/loginPage")}
-    >
-      <Text style={styles.text}>Login</Text>
-    </TouchableOpacity>
-  );
+    const [fontsLoaded] = useFonts({
+        FugazOne_400Regular,
+    })
+
+    if (fontsLoaded) {
+        return (
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => router.push("/pages/loginPage")}
+            >
+                <Text style={styles.text}>Login</Text>
+            </TouchableOpacity>
+        )
+    } else return <></>
 }
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 40,
-    margin: 25,
-    color: "white",
-  },
-  button: {
-    backgroundColor: "#8EC255",
-    margin: 20,
-    padding: 5,
-    width: 220,
-    alignSelf: "center",
-    height: 55,
-  },
-  text: {
-    color: "#ffffff",
-    alignSelf: "center",
-    fontFamily: "FugazOne_400Regular",
-    fontSize: 30,
-  },
-});
+    title: {
+        fontSize: 40,
+        margin: 25,
+        color: "white",
+    },
+    button: {
+        backgroundColor: "#8EC255",
+        margin: 20,
+        paddingTop: 5,
+        width: 220,
+        alignSelf: "center",
+        height: 55,
+    },
+    text: {
+        color: "#ffffff",
+        alignSelf: "center",
+        fontFamily: "FugazOne_400Regular",
+        fontSize: 30,
+    },
+})
--- app/components/WelcomeComponents/TitleArea.jsx
+++ app/components/WelcomeComponents/TitleArea.jsx
@@ -1,62 +1,67 @@
-import { Text, View, ImageBackground, StyleSheet } from "react-native";
-import { useFonts } from "expo-font";
-import { FugazOne_400Regular } from "@expo-google-fonts/fugaz-one";
+import { Text, View, ImageBackground, StyleSheet } from "react-native"
+import { useFonts } from "expo-font"
+import { FugazOne_400Regular } from "@expo-google-fonts/fugaz-one"
 
 export default function TitleArea() {
-  const [fontsLoaded] = useFonts({
-    FugazOne_400Regular,
-  });
-  const image = require("../../../assets/images/growing-plant.jpg");
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Start growing with the Plant Zone!</Text>
+    const [fontsLoaded] = useFonts({
+        FugazOne_400Regular,
+    })
+    const image = require("../../../assets/images/growing-plant.jpg")
 
-      <View style={styles.contentContainer}>
-        <ImageBackground //PLACEHOLDER FOR OUR LOGO
-          source={image}
-          style={styles.image}
-        />
-      </View>
-    </View>
-  );
+    if (fontsLoaded) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Start growing with the Plant Zone!
+                </Text>
+
+                <View style={styles.contentContainer}>
+                    <ImageBackground source={image} style={styles.image} />
+                </View>
+            </View>
+        )
+    } else {
+        return <></>
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#E8F3DD",
-    paddingHorizontal: 20,
-  },
-  title: {
-    fontSize: 30,
-    margin: 20,
-    marginBottom: 10,
-    color: "#80AF4D",
-    fontFamily: "FugazOne_400Regular",
-    textAlign: "center",
-    height: 130,
-  },
-  contentContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  image: {
-    width: 220,
-    height: 220,
-    margin: 15,
-  },
-  button: {
-    backgroundColor: "white",
-    alignItems: "center",
-    paddingVertical: 15,
-    paddingHorizontal: 10,
-    marginVertical: 15,
-    width: "50%",
-    borderRadius: 5,
-  },
-  textButton: {
-    fontSize: 25,
-    fontWeight: "bold",
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#E8F3DD",
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontSize: 30,
+        margin: 10,
+        marginTop: 30,
+        marginBottom: 10,
+        color: "#80AF4D",
+        fontFamily: "FugazOne_400Regular",
+        textAlign: "center",
+        height: 130,
+    },
+    contentContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    image: {
+        width: 220,
+        height: 220,
+        margin: 15,
+    },
+    button: {
+        backgroundColor: "white",
+        alignItems: "center",
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        marginVertical: 15,
+        width: "50%",
+        borderRadius: 5,
+    },
+    textButton: {
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+})
