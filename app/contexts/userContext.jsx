604a1963be65a07997dbdc88c552c31a7c3eb1f0
“Hayp” <hanna.y.peters@gmail.com>
2025-03-12 09:30:52 +0000

    user id in context

--- app/contexts/userContext.jsx
+++ app/contexts/userContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useState } from "react";
 
 const UserContext = createContext({
   user: {
+    id: 3,
     username: "AdrienneGemini",
     email: "Adrienne_Brown43@hotmail.com",
     location: "[51.996511955730675, -1.0707122543715286]",
@@ -10,6 +11,7 @@ const UserContext = createContext({
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState({
+    id: 3,
     username: "AdrienneGemini",
     email: "Adrienne_Brown43@hotmail.com",
     location: "[51.996511955730675, -1.0707122543715286]",
